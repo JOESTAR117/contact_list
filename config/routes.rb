a94a61a5369bf1ac1da22732855d1e848d1465b3
Joestar117 <juniorrios1231@gmail.com>
2023-03-27 17:24:05 -0300

    feat: user new form created and validation user created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :contacts
   resources :static_pages, only: %i[index about contact]
+  resources :users, only: %i[new create]
 
   get 'about', to: 'static_pages#about'
   get 'contato', to: 'static_pages#contato'
