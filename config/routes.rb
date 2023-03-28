b97ed3d6d1a00778e69de17ff7bcf8e0f629e8a4
Joestar117 <juniorrios1231@gmail.com>
2023-03-28 13:39:35 -0300

    feat: login,logout and page show user , created and finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   resources :contacts
   resources :static_pages, only: %i[index about contact]
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create show]
 
   get 'about', to: 'static_pages#about'
   get 'contato', to: 'static_pages#contato'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 
   root 'static_pages#index'
 end
